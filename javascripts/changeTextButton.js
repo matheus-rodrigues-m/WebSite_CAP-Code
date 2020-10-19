42a793b9592e509b9d4096bff6eddf9d16a21e97
matheus-rodrigues-m <matheusrodriguessmaia0318@gmail.com>
2020-10-19 15:39:03 -0300

    WebSite - CAP Code #1

---
+++ javascripts/changeTextButton.js
@@ -0,0 +1,14 @@
+function funcaoObj() {
+    document.getElementById("p_equipe").innerHTML = "Promover treinamentos de programação para todos os membros; <br>Participar de competições e maratonas de programação; <br> Conquistar resultados como uma equipe de competição; <br> Capacitar os alunos do núcleo na área de programação; <br> Promover aos alunos uma atividade extensionista;<br> Auxiliar pra promoção de horas como atividades extracurricular; <br> Trazer maior visibilidade ao Campus Alto Paraopeba da UFSJ. ";
+    document.getElementById("eqp").innerHTML = "Objetivos";
+}
+
+function funcaoVal() {
+    document.getElementById("p_equipe").innerHTML = "Cooperação e crescimento coletivo; <br>Responsabilidade e compromisso; <br>Superação de desafios; <br>Capacitação dos membros; <br>Busca por conhecimento; <br><br><br>";
+    document.getElementById("eqp").innerHTML = "Valores";
+}
+
+function funcaoVis() {
+    document.getElementById("p_equipe").innerHTML = "Reconhecimento como programadores; <br>Reconhecimento como equipe de competição; <br>Estarmos preparados para o mercado de trabalho; <br><br><br><br><br>";
+    document.getElementById("eqp").innerHTML = "Visão";
+}
